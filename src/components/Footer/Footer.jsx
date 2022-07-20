e9cfe1e418b5d6165ee54a4cc4f66132698304f3
George <letow@vk.com>
2022-07-20 21:46:12 +0500

    Added Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,24 @@
+﻿import React from 'react'
+import classes from './Footer.module.scss'
+import amex from './logo/amex.svg'
+import applepay from './logo/apple-pay.svg'
+import discover from './logo/discover.svg'
+import mastercard from './logo/mastercard.svg'
+import paypal from './logo/paypal.svg'
+import visa from './logo/visa.svg'
+
+export default function Footer() {
+  return (
+    <div className={classes.Footer}>
+        <span className={classes.copyright}>Copyright © 2022, <a className='link' href='#'>Polyphia Tabs</a>.</span>
+        <div className={classes.paylogo}>
+            <img className={classes.paylogo_item} src={amex} alt="amex" />
+            <img className={classes.paylogo_item} src={applepay} alt="applepay" />
+            <img className={classes.paylogo_item} src={discover} alt="discover" />
+            <img className={classes.paylogo_item} src={mastercard} alt="mastercard" />
+            <img className={classes.paylogo_item} src={paypal} alt="paypal" />
+            <img className={classes.paylogo_item} src={visa} alt="visa" />
+        </div>
+    </div>
+  )
+}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,15 +6,15 @@ export default function Header() {
     <div className={classes.Header}>
         <hr />
         <form action="#" className={classes.search_form}>
-          <a href='#' className={`material-symbols-outlined ${classes.icon} ${classes.icon_search}`}>search</a>
+          <a href='#' className={`material-symbols-outlined ${classes.icon} ${classes.icon_search} link`}>search</a>
           <input type="search" name="search" className={classes.search_bar} placeholder='Search'/>
         </form>
         <div className={classes.right}>
           <div className={classes.cart}>
-            <a href="#" className={classes.cart_text}><span class={`material-symbols-outlined ${classes.icon} ${classes.icon_cart}`}>shopping_cart</span><span>Cart (0)</span></a>
+            <a href="#" className={`${classes.cart_text} link`}><span class={`material-symbols-outlined ${classes.icon} ${classes.icon_cart}`}>shopping_cart</span><span>Cart (0)</span></a>
           </div>
           <div className={classes.checkout}>
-          <a href="#" className={classes.cart_text}>Check Out</a>
+          <a href="#" className={`${classes.cart_text} link`}>Check Out</a>
           </div>
         </div>
     </div>
