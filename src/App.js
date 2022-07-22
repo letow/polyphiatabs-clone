74f6d8115272ba696ba0df3c9d9d3038a52ea90b
George <letow@vk.com>
2022-07-22 13:34:26 +0500

    sidebar almost done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.scss';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Main from './components/Main/Main';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Main/>
       <Footer/>
     </div>
   );
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,19 @@
+﻿import React from 'react'
+import classes from './Main.module.scss'
+import facebook from './logo/facebook.svg'
+import twitter from './logo/twitter.svg'
+import instagram from './logo/instagram.svg'
+import youtube from './logo/youtube.svg'
+
+export default function Main() {
+  return (
+    <div className={classes.Main}>
+        <div className={classes.sidebar}>
+            <a href="#"><img className={classes.sidebar_icon} src={facebook} alt="facebook" /></a>
+            <a href="#"><img className={classes.sidebar_icon} id={classes.twitter} src={twitter} alt="twitter" /></a>
+            <a href="#"><img className={classes.sidebar_icon} src={instagram} alt="instagram" /></a>
+            <a href="#"><img className={classes.sidebar_icon} src={youtube} alt="youtube" /></a>
+        </div>
+    </div>
+  )
+}
