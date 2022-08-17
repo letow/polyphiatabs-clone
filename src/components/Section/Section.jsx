e919111a0fc4c3c3ab9e78c54efd9eef10e0d323
George <letow@vk.com>
2022-08-18 01:18:11 +0500

    Section items done

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -4,17 +4,19 @@ import SectionItem from './SectionItem/SectionItem'
 
 export default function Section({sectionTitle}) {
   const [sectionItems, setSectionItems] = useState([
-    'Neurotica Tabs',
-    'Playing God Tabs',
-    'Guitar Tab Bundle',
+    {name: 'Neurotica Tabs', img: 'https://cdn.shopify.com/s/files/1/2262/7457/products/NEUROTICA_RECOLOR_360x.jpg?v=1658155012', price: 3.00},
+    {name: 'Playing God Tabs', img: 'https://cdn.shopify.com/s/files/1/2262/7457/products/POLYPHIA-PLAYINGGOD-DSP_360x.jpg?v=1652194022', price: 3.00},
+    {name: 'Guitar Tab Bundle', img: 'https://cdn.shopify.com/s/files/1/2262/7457/products/guitartabbundle_360x.webp?v=1652205407', price: 50.00},
   ])
 
   return (
     <div className={classes.Section}>
         <h2 className={classes.header2}>{sectionTitle}</h2>
-        {sectionItems.map((item) => 
-          <SectionItem title={item} />  
-        )}
+        <div className={classes.wrapper}>
+          {sectionItems.map((data) => 
+            <SectionItem data={data} />  
+          )}
+        </div>
     </div>
   )
 }
--- src/components/Section/SectionItem/SectionItem.jsx
+++ src/components/Section/SectionItem/SectionItem.jsx
@@ -1,10 +1,12 @@
 ﻿import React from 'react'
 import classes from './SectionItem.module.scss'
 
-export default function SectionItem({title}) {
+export default function SectionItem(props) {
   return (
     <div className={classes.SectionItem}>
-        <p>Polyphia - {title}</p>
+        <a href='#' className='link'><img src={props.data.img} alt="cover" className={classes.cover} /></a>
+        <a href='#' className='link'><p className={classes.price}>Polyphia - {props.data.name}</p></a>
+        <strong><p className={classes.price}>${props.data.price.toFixed(2)}</p></strong>
     </div>
   )
 }
