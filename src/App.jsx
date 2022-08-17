e22c8a36b85b59ed7b0e8dfb6ed3dbde10f06b0f
George <letow@vk.com>
2022-08-17 22:28:59 +0500

    Fixed useState imports
    Signed-off-by: George <letow@vk.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './App.scss';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,4 +1,4 @@
-﻿import React from 'react'
+﻿import React, { useState } from 'react'
 import classes from './Section.module.scss'
 import SectionItem from './SectionItem/SectionItem'
 
